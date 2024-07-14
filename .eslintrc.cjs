128a0546064d81778101a3730d9a949889566566
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-07-15 04:46:51 +0700

    refactor [.eslintrc.cjs]: added rules to disable prop-types warning and unused-vars warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "react-hooks/exhaustive-deps": "off",
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
   },
 }
