29718a9e7b0e72e9599c0a44c5ce93b57779714b
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-07-15 04:50:29 +0700

    feat [Footer]: added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <>
+      <footer className=" bg-white border-t-2 border-gray-100">
+        <div className="max-w-7xl mx-auto px-4">
+          <div className="py-8 text-center space-y-2">
+            <h1 className="text-l font-semibold text-dark">Copyright © 2024</h1>
+            <p className="text-dark font-semibold">BolangTravel</p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
