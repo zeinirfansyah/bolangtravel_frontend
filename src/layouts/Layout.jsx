c557f40e9ebeb2c2cea199616e220f7ea8aa16a0
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-07-15 21:05:44 +0700

    refactor [Layout.jsx: make window scroll to top everytime changed page

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import { useLocation } from "react-router-dom";
 
 export const Layout = ({ children }) => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Header />
