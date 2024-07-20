2e124c93eab78e527e8895c316408d171c1f8067
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-07-20 11:11:03 +0700

    refactor [Dashboard.jsx]: added dynamic displayed data and fixed layout

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -65,101 +65,113 @@ export const Dashboard = () => {
       <div className="flex bg-slate-100">
         <div className="w-full pt-8">
           <div className="max-w-7xl mx-auto px-10">
-            <div className="min-h-screen flex flex-col space-y-6">
+            <div className="min-h-[90vh] flex flex-col space-y-6">
               <div className="flex flex-col gap-6">
-                <div className="flex flex-grow gap-4 items-center">
-                  <p>Show</p>
-                  <SelectOption
-                    options={limitData}
-                    default_value={5}
-                    onChange={(e) => setLimit(e.target.value)}
-                    firstOption="5"
-                    className={"w-[80px] border-2 rounded-lg p-2"}
-                  />
-                  <p>Entries</p>
-                </div>
-                <div className="flex flex-col items-center justify-center">
+                <div className="flex flex-col items-center justify-center max-h-[85vh]">
                   <div className="flex w-full my-3 gap-4">
-                    <Input
-                      placeholder="Search booking data by customer"
-                      onChange={(e) => {
-                        setSearch(e.target.value);
-                      }}
-                      value={search}
-                      className={"border-2"}
-                    />
-                    <SelectOption
-                      options={uniqueStatus}
-                      default_value=""
-                      onChange={(e) => setFilterByStatus(e.target.value)}
-                      firstOption="All Status"
-                      className={"w-1/3 border-2"}
-                    />
+                    <div className="w-full">
+                      <Input
+                        placeholder="Search booking data by customer"
+                        onChange={(e) => {
+                          setSearch(e.target.value);
+                        }}
+                        value={search}
+                        className={"border-2"}
+                      />
+                    </div>
+                    <div className="w-1/3">
+                      <SelectOption
+                        options={uniqueStatus}
+                        default_value=""
+                        onChange={(e) => setFilterByStatus(e.target.value)}
+                        firstOption="All Status"
+                        className={"border-2"}
+                      />
+                    </div>
                   </div>
-                  <table className="table-auto bg-white rounded-xl text-center text-sm border-2">
-                    <thead className="border-b">
-                      <tr>
-                        <th className="p-4">Date</th>
-                        <th className="p-4">User</th>
-                        <th className="p-4">Package</th>
-                        <th className="p-4">Total</th>
-                        <th className="p-4">Transfer Receipt</th>
-                        <th className="p-4">Status</th>
-                        <th className="p-4">Action</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {bookingList?.map((booking) => (
-                        <tr key={booking?.id}>
-                          <td className="p-4">{dayjs(booking?.date).format("DD MMM YYYY")}</td>
-                          <td className="p-4">{booking?.users?.username}</td>
-                          <td className="p-4">
-                            {booking?.travel_packages?.title}
-                          </td>
-                          <td className="p-4">
-                            {`IDR.${
-                              booking?.travel_packages?.price +
-                              booking?.travel_packages?.price * 0.11
-                            }`}
-                          </td>
-                          {booking?.transfer_receipt ? (
-                            <td className="p-4 min-w-[200px]">
-                              {booking?.transfer_receipt}
-                            </td>
-                          ) : (
-                            <td className="p-4 text-gray-400 min-w-[200px]">
-                              No receipt yet
-                            </td>
-                          )}
-                          <td className="p-4 min-w-[200px]">
-                            {booking?.status}
-                          </td>
-                          <td className="flex gap-2 p-4 min-w-[200px]">
-                            <Button className="">Edit</Button>
-                            <Button className="bg-red-500 text-white">
-                              Delete
-                            </Button>
-                          </td>
+                  <div className="w-full overflow-x-auto">
+                    <table className="table-auto bg-white rounded-xl text-center text-sm w-full overflow-x-scroll">
+                      <thead className="border-b">
+                        <tr>
+                          <th className="p-4">Date</th>
+                          <th className="p-4">User</th>
+                          <th className="p-4">Package</th>
+                          <th className="p-4">Total</th>
+                          <th className="p-4">Transfer Receipt</th>
+                          <th className="p-4">Status</th>
+                          <th className="p-4">Action</th>
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                  <div className="flex items-center space-x-4 my-8">
-                    <button
-                      onClick={() => setPage(page - 1)}
-                      disabled={page === 1}
-                      className={`px-4 py-2 border rounded-md text-white bg-blue-500 hover:bg-blue-600 disabled:bg-gray-300 disabled:cursor-not-allowed transition-colors duration-200`}
-                    >
-                      Previous
-                    </button>
-                    <span className="text-lg font-semibold">{page}</span>
-                    <button
-                      onClick={() => setPage(page + 1)}
-                      disabled={bookingList.length < limit}
-                      className={`px-4 py-2 border rounded-md text-white bg-blue-500 hover:bg-blue-600 disabled:bg-gray-300 disabled:cursor-not-allowed transition-colors duration-200`}
-                    >
-                      Next
-                    </button>
+                      </thead>
+                      <tbody>
+                        {bookingList?.map((booking) => (
+                          <tr key={booking?.id}>
+                            <td className="p-4">
+                              {dayjs(booking?.date).format("DD MMM YYYY")}
+                            </td>
+                            <td className="p-4">{booking?.users?.username}</td>
+                            <td className="p-4">
+                              {booking?.travel_packages?.title}
+                            </td>
+                            <td className="p-4">
+                              {`IDR.${
+                                booking?.travel_packages?.price +
+                                booking?.travel_packages?.price * 0.11
+                              }`}
+                            </td>
+                            {booking?.transfer_receipt ? (
+                              <td className="p-4 min-w-[200px]">
+                                {booking?.transfer_receipt}
+                              </td>
+                            ) : (
+                              <td className="p-4 text-gray-400 min-w-[200px]">
+                                No receipt yet
+                              </td>
+                            )}
+                            <td className="p-4 min-w-[200px]">
+                              {booking?.status}
+                            </td>
+                            <td className="flex gap-2 p-4 min-w-[200px]">
+                              <Button className="">Edit</Button>
+                              <Button className="bg-red-500 text-white">
+                                Delete
+                              </Button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                  <div className="flex justify-between w-full">
+                    <div className="flex flex-grow gap-4 items-center">
+                      <p>Show</p>
+                      <div className="max-w-[120px]">
+                        <SelectOption
+                          options={limitData}
+                          default_value={5}
+                          onChange={(e) => setLimit(e.target.value)}
+                          firstOption="5"
+                          className={"border-2 rounded-lg py-2"}
+                        />
+                      </div>
+                      <p>Entries</p>
+                    </div>
+                    <div className="flex items-center space-x-4 my-8">
+                      <button
+                        onClick={() => setPage(page - 1)}
+                        disabled={page === 1}
+                        className={`px-4 py-2 border rounded-md text-white bg-blue-500 hover:bg-blue-600 disabled:bg-gray-300 disabled:cursor-not-allowed transition-colors duration-200`}
+                      >
+                        Previous
+                      </button>
+                      <span className="text-lg font-semibold">{page}</span>
+                      <button
+                        onClick={() => setPage(page + 1)}
+                        disabled={bookingList.length < limit}
+                        className={`px-4 py-2 border rounded-md text-white bg-blue-500 hover:bg-blue-600 disabled:bg-gray-300 disabled:cursor-not-allowed transition-colors duration-200`}
+                      >
+                        Next
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
