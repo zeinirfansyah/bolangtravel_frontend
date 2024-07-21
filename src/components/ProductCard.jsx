2cce3cafd5beee5ff1c063f5721c4185f4026d85
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-07-21 21:37:11 +0700

    feat [ProductCard.jsx]: added  product card component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,57 @@
+import LinesEllipsis from "react-lines-ellipsis";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { DOMAIN_URL } from "../hooks/useApi";
+
+const ProductCard = ({ product }) => {
+  return (
+    <>
+      <Link
+        key={product.id}
+        to={`/paket-wisata/${product.id}`}
+        className="min-h-[240px] my-3"
+      >
+        <picture className="relative hover:brightness-75 transition-all duration-300">
+          <div className="absolute right-0 top-0 rounded-bl-xl rounded-tr-xl">
+            <div className="flex items-center">
+              <p className="text-white text-xs md:text-sm font-semibold bg-primary px-3 py-1 w-fit rounded-bl-xl rounded-tr-xl capitalize">
+                {product?.category}
+              </p>
+            </div>
+          </div>
+          <img
+            src={`${DOMAIN_URL}/${product?.thumbnail}`}
+            data-size="auto"
+            alt="Foto Destinasi Tanjung Benoa"
+            className="mb-3 lazyload rounded-xl object-cover h-52 w-full lg:w-80 lazyloaded"
+          />
+        </picture>
+        <div className="flex flex-col">
+          <div className="items-center text-xs">
+            <p>{product?.location}</p>
+          </div>
+          <LinesEllipsis
+            text={product.title}
+            maxLine="2"
+            ellipsis="..."
+            trimRight
+            basedOn="letters"
+            className="text-dark font-medium text-base lg:text-base "
+          />
+          <p className="text-slate-400 text-sm">
+            {product.price} / {product.duration} Days
+          </p>
+          <p className="text-slate-400 text-sm">
+            {product.tour_location}
+          </p>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+ProductCard.propTypes = {
+  product: PropTypes.object.isRequired,
+};
+
+export default ProductCard;
